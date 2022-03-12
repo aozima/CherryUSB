389c912d4778c097ae69858ab72f45ff76914454
aozima <wuyangyong@gmail.com>
2022-03-12 16:26:04 +0800

    初始化端点属性

--- class/wireless/usbh_rndis.c
+++ class/wireless/usbh_rndis.c
@@ -9,7 +9,7 @@ static int usbh_rndis_connect(struct usbh_hubport *hport, uint8_t intf)
     struct usbh_endpoint_cfg ep_cfg = { 0 };
     struct usb_endpoint_descriptor *ep_desc;
 
-    USB_LOG_ERR("TBD: %s %d\r\n", __FUNCTION__, __LINE__);
+    USB_LOG_INFO("%s %d\r\n", __FUNCTION__, __LINE__);
 
     struct usbh_rndis *class = usb_malloc(sizeof(struct usbh_rndis));
     if (class == NULL) 
@@ -24,7 +24,7 @@ static int usbh_rndis_connect(struct usbh_hubport *hport, uint8_t intf)
     hport->config.intf[intf].priv = class;
 
 #if 1
-    USB_LOG_INFO("intf_desc.bNumEndpoints:%d\r\n", hport->config.intf[intf].intf_desc.bNumEndpoints);
+    USB_LOG_INFO("hport=%p, intf=%d, intf_desc.bNumEndpoints:%d\r\n", hport, intf, hport->config.intf[intf].intf_desc.bNumEndpoints);
     for (uint8_t i = 0; i < hport->config.intf[intf].intf_desc.bNumEndpoints; i++) 
     {
         ep_desc = &hport->config.intf[intf].ep[i].ep_desc;
@@ -44,10 +44,22 @@ static int usbh_rndis_data_connect(struct usbh_hubport *hport, uint8_t intf)
     struct usbh_endpoint_cfg ep_cfg = { 0 };
     struct usb_endpoint_descriptor *ep_desc;
 
-    USB_LOG_ERR("TBD: %s %d\r\n", __FUNCTION__, __LINE__);
+    USB_LOG_INFO("%s %d\r\n", __FUNCTION__, __LINE__);
+
+    struct usbh_rndis *class = usb_malloc(sizeof(struct usbh_rndis));
+    if (class == NULL) 
+    {
+        USB_LOG_ERR("Fail to alloc class\r\n");
+        return -ENOMEM;
+    }
+    memset(class, 0, sizeof(struct usbh_rndis));
+
+    snprintf(hport->config.intf[intf].devname, CONFIG_USBHOST_DEV_NAMELEN, DEV_FORMAT, intf);
+    USB_LOG_INFO("Register RNDIS Class:%s\r\n", hport->config.intf[intf].devname);
+    hport->config.intf[intf].priv = class;
 
 #if 1
-    USB_LOG_INFO("intf_desc.bNumEndpoints:%d\r\n", hport->config.intf[intf].intf_desc.bNumEndpoints);
+    USB_LOG_INFO("hport=%p, intf=%d, intf_desc.bNumEndpoints:%d\r\n", hport, intf, hport->config.intf[intf].intf_desc.bNumEndpoints);
     for (uint8_t i = 0; i < hport->config.intf[intf].intf_desc.bNumEndpoints; i++) 
     {
         ep_desc = &hport->config.intf[intf].ep[i].ep_desc;
@@ -57,6 +69,22 @@ static int usbh_rndis_data_connect(struct usbh_hubport *hport, uint8_t intf)
         USB_LOG_INFO("wMaxPacketSize=%d, bInterval=%d\r\n\r\n", ep_desc->wMaxPacketSize, ep_desc->bInterval);
     }
 #endif
+
+    for (uint8_t i = 0; i < hport->config.intf[intf + 1].intf_desc.bNumEndpoints; i++) 
+    {
+        ep_desc = &hport->config.intf[intf + 1].ep[i].ep_desc;
+
+        ep_cfg.ep_addr = ep_desc->bEndpointAddress;
+        ep_cfg.ep_type = ep_desc->bmAttributes & USB_ENDPOINT_TYPE_MASK;
+        ep_cfg.ep_mps = ep_desc->wMaxPacketSize;
+        ep_cfg.ep_interval = ep_desc->bInterval;
+        ep_cfg.hport = hport;
+        if (ep_desc->bEndpointAddress & 0x80) {
+            usbh_ep_alloc(&class->bulkin, &ep_cfg);
+        } else {
+            usbh_ep_alloc(&class->bulkout, &ep_cfg);
+        }
+    }
 
     return ret;
 }
