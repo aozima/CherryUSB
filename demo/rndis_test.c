4da6f3a084d53b4df938cf3d16aedd5a4f7346f8
aozima <wuyangyong@gmail.com>
2022-03-13 22:35:56 +0800

    添加以太网驱动

--- demo/rndis_test.c
+++ demo/rndis_test.c
@@ -199,6 +199,109 @@ static int rndis_query_oid(struct usbh_rndis *class, uint32_t oid, int query_len
     return ret;
 }
 
+#include <netif/ethernetif.h>
+#include <netdev.h>
+
+#define MAX_ADDR_LEN         6
+
+struct rt_rndis_eth
+{
+    /* inherit from ethernet device */
+    struct eth_device parent;
+
+    struct usbh_hubport *hport;
+    uint8_t intf;
+
+    rt_uint8_t   dev_addr[MAX_ADDR_LEN];
+};
+typedef struct rt_rndis_eth * rt_rndis_eth_t;
+static struct rt_rndis_eth usbh_rndis_eth_device;
+
+static rt_err_t rt_rndis_eth_init(rt_device_t dev)
+{
+    return RT_EOK;
+}
+
+static rt_err_t rt_rndis_eth_open(rt_device_t dev, rt_uint16_t oflag)
+{
+    return RT_EOK;
+}
+
+static rt_err_t rt_rndis_eth_close(rt_device_t dev)
+{
+    return RT_EOK;
+}
+
+static rt_size_t rt_rndis_eth_read(rt_device_t dev, rt_off_t pos, void* buffer, rt_size_t size)
+{
+    rt_set_errno(-RT_ENOSYS);
+    return 0;
+}
+
+static rt_size_t rt_rndis_eth_write (rt_device_t dev, rt_off_t pos, const void* buffer, rt_size_t size)
+{
+    rt_set_errno(-RT_ENOSYS);
+    return 0;
+}
+static rt_err_t rt_rndis_eth_control(rt_device_t dev, int cmd, void *args)
+{
+    rt_rndis_eth_t rndis_eth_dev = (rt_rndis_eth_t)dev;
+
+    USB_LOG_INFO("%s L%d\r\n", __FUNCTION__, __LINE__);
+    switch(cmd)
+    {
+    case NIOCTL_GADDR:
+        /* get mac address */
+        if(args)
+        { 
+            USB_LOG_INFO("%s L%d NIOCTL_GADDR\r\n", __FUNCTION__, __LINE__);
+            rt_memcpy(args, rndis_eth_dev->dev_addr, MAX_ADDR_LEN);
+        }    
+        else
+        { 
+            return -RT_ERROR;
+        }    
+        break;
+    default :
+        break;
+    }
+
+    return RT_EOK;
+}
+
+/* reception packet. */
+struct pbuf *rt_rndis_eth_rx(rt_device_t dev)
+{
+    struct pbuf* p = RT_NULL;
+
+    USB_LOG_INFO("%s L%d\r\n", __FUNCTION__, __LINE__);
+
+    return p;
+}
+
+/* transmit packet. */
+rt_err_t rt_rndis_eth_tx(rt_device_t dev, struct pbuf* p)
+{
+    struct pbuf* q;
+    rt_err_t result = RT_EOK;
+
+    USB_LOG_INFO("%s L%d\r\n", __FUNCTION__, __LINE__);
+
+    return result;
+}
+
+#ifdef RT_USING_DEVICE_OPS
+const static struct rt_device_ops rndis_device_ops =
+{
+    rt_rndis_eth_init,
+    rt_rndis_eth_open,
+    rt_rndis_eth_close,
+    rt_rndis_eth_read,
+    rt_rndis_eth_write,
+    rt_rndis_eth_control
+}
+#endif /* RT_USING_DEVICE_OPS */
+
 static void rt_thread_rndis_data_entry(void *parameter)
 {
     int ret, len;
@@ -250,11 +353,32 @@ static void rt_thread_rndis_data_entry(void *parameter)
     {
         memcpy(&dhcp_discover_data[6], cdc_buffer, query_len);
         memcpy(&dhcp_discover_data[0x46], cdc_buffer, query_len);
+        memcpy(&usbh_rndis_eth_device.dev_addr[0], cdc_buffer, query_len);
         dump_hex(cdc_buffer, query_len);
     }
 
+#ifdef RT_USING_DEVICE_OPS
+    usbh_rndis_eth_device.parent.parent.ops           = &rndis_device_ops;
+#else
+    usbh_rndis_eth_device.parent.parent.init          = rt_rndis_eth_init;
+    usbh_rndis_eth_device.parent.parent.open          = rt_rndis_eth_open;
+    usbh_rndis_eth_device.parent.parent.close         = rt_rndis_eth_close;
+    usbh_rndis_eth_device.parent.parent.read          = rt_rndis_eth_read;
+    usbh_rndis_eth_device.parent.parent.write         = rt_rndis_eth_write;
+    usbh_rndis_eth_device.parent.parent.control       = rt_rndis_eth_control;
+#endif
+    usbh_rndis_eth_device.parent.parent.user_data     = RT_NULL;
+
+    usbh_rndis_eth_device.parent.eth_rx               = rt_rndis_eth_rx;
+    usbh_rndis_eth_device.parent.eth_tx               = rt_rndis_eth_tx;
+
+    eth_device_init(&usbh_rndis_eth_device.parent, "u0");
+
+    return;
+
     ret = rndis_keepalive(rndis_class);
     USB_LOG_INFO("rndis_keepalive ret=%d\r\n", ret);
+
 
 #if 0 
     USB_LOG_INFO("%s L%d\r\n", __FUNCTION__, __LINE__);
